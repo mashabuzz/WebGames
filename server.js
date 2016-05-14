1b9c141a7449f02d8d8e0b776c57bbda7d57f24b
Rahul Paul <rahulpaul.iitkgp@gmail.com>
2016-05-14 23:03:02 +0530

    yet another commit

--- server.js
+++ server.js
@@ -3,20 +3,33 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
 
 var app = express();
 
 app.use(express.static('static'));
 app.use(express.static(path.join(__dirname, 'bower_components')))
 
-// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
 
 app.post('/scores', (req, res) => {
-    console.log(req.body);
+    var userName = 'rahul.p';
+    var gameName = req.body.game;
+    var startTime = req.body.start_time;    
+    var endTime = req.body.end_time;
+    var score = req.body.score;
+    
+    var db = new sqlite3.Database('db/WebGames.db'); 
+    
+    db.serialize(function() {
+        // TODO handle error
+        var stmt = db.prepare('INSERT INTO SCORES VALUES(?, ?, ?, ?, ?)');
+        stmt.run(userName, gameName, startTime, endTime, score);
+        stmt.finalize();
+    });
+    
+    db.close();    
     res.send('');
 });
 
